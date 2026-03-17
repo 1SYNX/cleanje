6affeab9f80982923f9208254a240dc9e31a4de0
Cleanje <one.synxsolutions@gmail.com>
2026-03-17 17:37:48 +0800

    Add dropdown loading for booking fields

---
+++ booking.js
@@ -0,0 +1,5 @@
+loadDropdown('ref_customer_type', 'customerType');
+loadDropdown('ref_time_slot', 'timeSlot');
+loadDropdown('ref_booking_source', 'source');
+loadDropdown('ref_booking_status', 'status');
+loadDropdown('ref_technician_crew', 'crew');
